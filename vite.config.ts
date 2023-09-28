1a80cefac0ee1820d745871ce08359194eba4ef6
Geoffrey Litt <gklitt@gmail.com>
2023-09-28 18:25:28 -0400

    add base path to support trail-runner deployment (not sure if breaks netlify)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  base: "./",
   plugins: [topLevelAwait(), wasm(), react()],
 
   worker: {
