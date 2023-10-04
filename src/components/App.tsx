3ef6a9a203292c252694368b5f6f7d4e52c7e498
Geoffrey Litt <gklitt@gmail.com>
2023-10-04 14:11:49 -0400

    install shadcn/ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import { AutomergeUrl } from "@automerge/automerge-repo";
 import { useDocument, useHandle } from "@automerge/automerge-repo-react-hooks";
 import { MarkdownEditor } from "./MarkdownEditor";
+import { Button } from "@/components/ui/button";
 import { MarkdownDoc } from "../schema";
 
 function App({ docUrl }: { docUrl: AutomergeUrl }) {
@@ -13,14 +14,22 @@ function App({ docUrl }: { docUrl: AutomergeUrl }) {
 
   return (
     <div>
-      <div className="h-10 w-screen bg-gray-200 p-2 bg-gradient-to-b from-white to-gray-200 border-b border-gray-300">
-        Tiny Essay Editor
+      <div className="h-10 w-screen bg-gray-200 p-2 bg-gradient-to-b from-white to-gray-200 border-b border-gray-300 align-middle">
+        <img
+          className="h-full inline-block mr-1"
+          src="/assets/logo-favicon-310x310-transparent.png"
+        />
+        <div className="inline-block align-middle">Tiny Essay Editor</div>
       </div>
-      <div className="flex gap-8">
-        <div className="w-4/5 max-w-[776px]">
+      <div className="flex bg-gray-50">
+        <div className="w-4/5 max-w-[776px] bg-white my-4 mx-8 border border-gray-200 p-4 ">
           <MarkdownEditor handle={handle} path={["content"]} />
         </div>
-        <div className="flex-grow">Comments</div>
+        <div className="flex-grow bg-gray-50 p-4">
+          <Button className="" variant="outline">
+            Add a comment
+          </Button>
+        </div>
       </div>
     </div>
   );
