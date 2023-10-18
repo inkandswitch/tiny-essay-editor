10c5703481cabc7330784d1ddf6b4d7e9fbc1a92
Geoffrey Litt <gklitt@gmail.com>
2023-10-18 15:15:06 -0400

    working TOC

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        customGray: "#F2F2F2",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
