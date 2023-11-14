1bbf280124a9314f6355c3f8313ebc44dc25f121
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-11-14 10:27:04 +0100

    Show sync error if we have pending changes and the server is not responding

--- src/components/SyncIndicator.tsx
+++ src/components/SyncIndicator.tsx
@@ -4,16 +4,21 @@ import { useState, useEffect } from "react";
 import { useRepo } from "@automerge/automerge-repo-react-hooks";
 import { WifiIcon, WifiOffIcon } from "lucide-react";
 
+// if we are connected to the sync server and have outstanding changes we should switch to the error mode if we haven't received a sync message in a while
+// this variable specifies this timeout duration
+const SYNC_TIMEOUT = 3000 
+
 export const SyncIndicator = ({ handle }: { handle: DocHandle<unknown> }) => {
   // todo: sync shouldn't flicker. It should be based on if we are making progress syncing the changes.
   // While a user is actively editing the document their heads will never match the synced heads
   // is is fine and shouldn't be counted as out of sync as long as they are not lagging behind the sync server by too much
-  const isSynced = useIsSyncedWithServer(handle);
+  const {isSynced, lastSyncUpdate} = useSyncStateOfServer(handle);
   const isOnline = useIsOnline();
   const isConnectedToServer = useIsConnectedToServer();
+  const isTimedOut = useIsTimedOut(lastSyncUpdate, SYNC_TIMEOUT)
 
   if (isOnline) {
-    if (isConnectedToServer) {
+    if (isConnectedToServer && (isSynced || !isTimedOut)) {
       return (
         <div className="text-gray-500">
           <WifiIcon size={"20px"} className="inline-block mr-[7px]" />
@@ -38,13 +43,53 @@ export const SyncIndicator = ({ handle }: { handle: DocHandle<unknown> }) => {
   }
 };
 
+
 const SYNC_SERVER_PREFIX = "storage-server-";
 
-function useIsSyncedWithServer(handle: DocHandle<unknown>): boolean {
+function useIsTimedOut (timestamp: number, duration: number) {
+  const [isTimedOut, setIsTimedOut] = useState(getIsTimedOut(timestamp, duration))
+
+  useEffect(() => {
+    console.log(timestamp, duration, getIsTimedOut(timestamp, duration))
+
+    if (getIsTimedOut(timestamp, duration)) {
+      setIsTimedOut(true)
+      return
+    } 
+
+    setIsTimedOut(false)
+
+    const timeoutCallback = () => {  
+      setIsTimedOut(true)
+    }
+
+    const timeout = setTimeout(timeoutCallback, duration - (Date.now() - timestamp))
+
+    return () => {
+      setIsTimedOut(getIsTimedOut(timestamp, duration))
+      clearTimeout(timeout)
+    }
+  }, [timestamp, duration])
+
+
+  return isTimedOut
+}
+
+function getIsTimedOut (timestamp: number, timeoutDuration: number) : boolean {
+  return (Date.now() - timestamp) >= timeoutDuration
+}
+
+interface SyncState {
+  isSynced: boolean,
+  lastSyncUpdate: number
+}
+
+function useSyncStateOfServer(handle: DocHandle<unknown>): SyncState {
   const [currentHeads, setCurrentHeads] = useState(
     A.getHeads(handle.docSync())
   );
   const [syncedHeads, setSyncedHeads] = useState([]);
+  const [lastSyncUpdate, setLastSyncUpdate] = useState(Date.now()) // initialize to now, so we don't go into an error state before first sync
 
   useEffect(() => {
     handle.on("change", () => {
@@ -53,12 +98,18 @@ function useIsSyncedWithServer(handle: DocHandle<unknown>): boolean {
 
     handle.on("remote-heads", ({ peerId, heads }) => {
       if (peerId.match(SYNC_SERVER_PREFIX)) {
+        // todo: should add date to sync state
+        setLastSyncUpdate(Date.now()) 
         setSyncedHeads(heads);
       }
     });
   }, [handle]);
 
-  return arraysEqual(currentHeads, syncedHeads);
+
+  return {
+    isSynced : arraysEqual(currentHeads, syncedHeads),
+    lastSyncUpdate
+  }
 }
 
 export function arraysEqual(a, b) {
