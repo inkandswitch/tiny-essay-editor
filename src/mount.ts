3115f0e835b8004efb7d23f831f8478bc8903cf2
Geoffrey Litt <gklitt@gmail.com>
2023-11-21 16:47:42 -0500

    add a comment

--- src/mount.ts
+++ src/mount.ts
@@ -1,19 +1,23 @@
-import React from "react"
-import ReactDom from "react-dom/client"
-import App from "./components/App.js"
-import { RepoContext } from "@automerge/automerge-repo-react-hooks"
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./components/App.js";
+import { RepoContext } from "@automerge/automerge-repo-react-hooks";
 
-export function mount(node, params) { 
+export function mount(node, params) {
   // workaround different conventions for documentUrl
   if (!params.docUrl && params.documentUrl) {
-    params.docUrl = params.documentUrl
+    params.docUrl = params.documentUrl;
   }
 
   ReactDom.createRoot(node).render(
-    // eslint-disable-next-line no-undef 
-    // @ts-expect-error - repo is on window
-    React.createElement(RepoContext.Provider, { value: repo }, 
+    // We get the Automerge Repo from the global window;
+    // this is set by either our standalone entrypoint or trailrunner
+    React.createElement(
+      RepoContext.Provider,
+      // eslint-disable-next-line no-undef
+      // @ts-expect-error - repo is on window
+      { value: repo },
       React.createElement(App, Object.assign({}, params))
     )
-  ) 
+  );
 }
