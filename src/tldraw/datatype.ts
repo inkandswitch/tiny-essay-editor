4b89fdfe3319eff6a598dd961ad96558c272e4ae
Peter van Hardenberg <pvh@pvh.ca>
2024-01-30 12:40:07 -0800

    init and icon

--- src/tldraw/datatype.ts
+++ src/tldraw/datatype.ts
@@ -1,26 +1,27 @@
-import { init as tldrawinit } from "automerge-tldraw"
+import { init as tldrawinit } from "automerge-tldraw";
 
-import { PenBox } from "lucide-react"
+import { PenLine } from "lucide-react";
 
 // When a copy of the document has been made,
 // update the title so it's more clear which one is the copy vs original.
 // (this mechanism needs to be thought out more...)
 export const markCopy = (doc: any) => {
-  console.log("not implemented", doc)
-}
+  console.log("not implemented", doc);
+};
 
 const getTitle = (doc: any) => {
-  return doc.store["document:document"].name || "Drawing"
-}
+  return doc.store["document:document"].name || "Drawing";
+};
 
 const init = (doc: any) => {
-  return tldrawinit(doc)
-}
+  tldrawinit(doc);
+  doc.store["document:document"].name = "Drawing";
+};
 
 export const TLDrawDatatype = {
   id: "tldraw",
   name: "Drawing",
-  icon: PenBox,
+  icon: PenLine,
   init,
   getTitle,
-}
+};
