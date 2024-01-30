462d6fe29044f0b93196725a607d0bbc7cbb084a
Peter van Hardenberg <pvh@pvh.ca>
2024-01-30 15:14:38 -0800

    fix presence

--- src/tldraw/components/TLDraw.tsx
+++ src/tldraw/components/TLDraw.tsx
@@ -1,20 +1,22 @@
-import { AutomergeUrl } from "@automerge/automerge-repo"
-import { useDocument, useHandle } from "@automerge/automerge-repo-react-hooks"
+import { AutomergeUrl } from "@automerge/automerge-repo";
+import { useDocument, useHandle } from "@automerge/automerge-repo-react-hooks";
 
-import { TLDrawDoc } from "../schema"
-import { useAutomergeStore } from "automerge-tldraw"
-import { Tldraw } from "@tldraw/tldraw"
-import "@tldraw/tldraw/tldraw.css"
+import { TLDrawDoc } from "../schema";
+import { useAutomergeStore } from "automerge-tldraw";
+import { Tldraw } from "@tldraw/tldraw";
+import "@tldraw/tldraw/tldraw.css";
+import { useCurrentAccount } from "@/DocExplorer/account";
 
 export const TLDraw = ({ docUrl }: { docUrl: AutomergeUrl }) => {
-  useDocument<TLDrawDoc>(docUrl) // used to trigger re-rendering when the doc loads
-  const handle = useHandle<TLDrawDoc>(docUrl)
+  useDocument<TLDrawDoc>(docUrl); // used to trigger re-rendering when the doc loads
+  const handle = useHandle<TLDrawDoc>(docUrl);
+  const account = useCurrentAccount();
+  const userId = account ? account.contactHandle.url : "no-account";
 
-  const userId = "someone"
-  const store = useAutomergeStore({ handle, userId })
+  const store = useAutomergeStore({ handle, userId });
   return (
     <div className="tldraw__editor h-full overflow-auto">
       <Tldraw autoFocus store={store} />
     </div>
-  )
-}
+  );
+};
