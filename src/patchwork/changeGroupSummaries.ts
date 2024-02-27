6fa8477d7a8b0bb89865ca0250d9b59cb25d656b
Geoffrey Litt <gklitt@gmail.com>
2024-02-27 12:25:55 -0500

    dont repopulate existing summaries

--- src/patchwork/changeGroupSummaries.ts
+++ src/patchwork/changeGroupSummaries.ts
@@ -1,7 +1,6 @@
 import { DocHandle } from "@automerge/automerge-repo";
 import { ChangeGroup } from "./groupChanges";
 import { DiffWithProvenance, HasChangeGroupSummaries } from "./schema";
-import { Patch } from "@automerge/automerge-wasm";
 import { Doc } from "@automerge/automerge";
 import { getStringCompletion } from "@/llm";
 import { pick } from "lodash";
@@ -9,9 +8,11 @@ import { pick } from "lodash";
 export const populateChangeGroupSummaries = async ({
   groups,
   handle,
+  force,
 }: {
   groups: ChangeGroup[];
   handle: DocHandle<HasChangeGroupSummaries>;
+  force?: boolean;
 }) => {
   handle.change((doc) => {
     if (!doc.changeGroupSummaries) {
@@ -20,6 +21,9 @@ export const populateChangeGroupSummaries = async ({
   });
 
   for (const [index, group] of groups.entries()) {
+    if (!force && handle.docSync().changeGroupSummaries[group.id]) {
+      continue;
+    }
     const docBefore = groups[index - 1]?.docAtEndOfChangeGroup ?? {};
     const docAfter = group.docAtEndOfChangeGroup;
     const summary = await autoSummarizeGroup({
