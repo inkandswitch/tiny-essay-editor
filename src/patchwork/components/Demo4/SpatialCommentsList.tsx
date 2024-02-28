9258bfa11dd1942c80e4e102e460200cca08cab1
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-02-28 14:54:50 +0100

    Allow to reply / resolve comments with keyboard shortcuts

--- src/patchwork/components/Demo4/SpatialCommentsList.tsx
+++ src/patchwork/components/Demo4/SpatialCommentsList.tsx
@@ -80,6 +80,9 @@ export const SpatialCommentsList = React.memo(
     };
 
     const replyToDiscussion = (discussion: Discussion) => {
+      setActiveReplyThreadId(null);
+      setPendingCommentText("");
+
       changeDoc((doc) => {
         doc.discussions[discussion.id].comments.push({
           id: uuid(),
@@ -91,7 +94,11 @@ export const SpatialCommentsList = React.memo(
     };
 
     const resolveDiscussion = (discussion: Discussion) => {
-      console.log("resolve discussion");
+      const index = discussions.findIndex((d) => d.id === discussion.id);
+      const nextDiscussion = discussion[index + 1];
+
+      setSelectedDiscussionId(nextDiscussion ? nextDiscussion.id : undefined);
+
       changeDoc((doc) => {
         doc.discussions[discussion.id].resolved = true;
       });
@@ -129,7 +136,13 @@ export const SpatialCommentsList = React.memo(
       });
     }, [topDiscussion, scrollContainer]);
 
-    // handle navigation shortcuts
+    // handle keyboard shortcuts
+    /*
+     * k / ctrl + p / cmd + p : select previous discussion
+     * j / ctrl + n / cmd + n: select next discussion
+     * cmd + y / ctrl + y: resolve discussion
+     * cmd + enter / ctrl + enter : reply
+     */
     useEffect(() => {
       if (!selectedDiscussionId) {
         return;
@@ -141,8 +154,12 @@ export const SpatialCommentsList = React.memo(
         const currentIndex = discussions.findIndex(
           (discussion) => discussion.id === selectedDiscussionId
         );
+        const currentDiscussion = discussions[currentIndex];
 
-        if (evt.key === "k" || (evt.key === "p" && evt.ctrlKey)) {
+        const isMetaOrControlPressed = evt.ctrlKey || evt.metaKey;
+
+        // select previous discussion
+        if (evt.key === "k" || (evt.key === "p" && isMetaOrControlPressed)) {
           if (currentIndex > 0) {
             setSelectedDiscussionId(discussions[currentIndex - 1].id);
             evt.preventDefault();
@@ -152,7 +169,8 @@ export const SpatialCommentsList = React.memo(
           return;
         }
 
-        if (evt.key === "j" || (evt.key === "n" && evt.ctrlKey)) {
+        // select next discussion
+        if (evt.key === "j" || evt.key === "n") {
           if (currentIndex < discussions.length - 1) {
             setSelectedDiscussionId(discussions[currentIndex + 1].id);
             evt.preventDefault();
@@ -160,6 +178,22 @@ export const SpatialCommentsList = React.memo(
           }
 
           return;
+        }
+
+        if (evt.key === "y" && isMetaOrControlPressed) {
+          resolveDiscussion(currentDiscussion);
+          evt.preventDefault();
+          evt.stopPropagation();
+        }
+
+        if (
+          evt.key === "Enter" &&
+          isMetaOrControlPressed &&
+          !activeReplyThreadId
+        ) {
+          setActiveReplyThreadId(selectedDiscussionId);
+          evt.preventDefault();
+          evt.stopPropagation();
         }
       };
 
@@ -169,6 +203,16 @@ export const SpatialCommentsList = React.memo(
         window.removeEventListener("keydown", onKeydown);
       };
     }, [selectedDiscussionId]);
+
+    // hack to unblur button
+    useEffect(() => {
+      if (!activeReplyThreadId) {
+        console.log("blur");
+        setTimeout(() => {
+          (document.activeElement as HTMLElement).blur();
+        }, 200);
+      }
+    }, [activeReplyThreadId]);
 
     return (
       <div
@@ -195,7 +239,7 @@ export const SpatialCommentsList = React.memo(
                 }}
                 onClick={() => setSelectedDiscussionId(discussion.id)}
                 key={discussion.id}
-                className={`select-none mr-2 px-2 py-1 border rounded-sm hover:bg-gray-50  hover:border-gray-400              
+                className={`select-none mr-2 px-2 py-1 border rounded-sm  hover:border-gray-400              
                 ${
                   topDiscussion &&
                   topDiscussion.discussion.id === discussion.id &&
@@ -237,54 +281,65 @@ export const SpatialCommentsList = React.memo(
                     </div>
                   ))}
                 </div>
-                <div className="mt-1">
-                  <Popover
-                    open={activeReplyThreadId === discussion.id}
-                    onOpenChange={(open) => {
-                      open
-                        ? setActiveReplyThreadId(discussion.id)
-                        : setActiveReplyThreadId(null);
-                    }}
-                  >
-                    <PopoverTrigger asChild>
-                      <Button className="mr-2 px-2 h-8" variant="outline">
-                        <Reply className="mr-2" /> Reply
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent>
-                      <Textarea
-                        className="mb-4"
-                        value={pendingCommentText}
-                        onChange={(event) =>
-                          setPendingCommentText(event.target.value)
-                        }
-                        onKeyDown={(event) => {
-                          if (event.key === "Enter" && event.metaKey) {
-                            replyToDiscussion(discussion);
-                            event.preventDefault();
-                          }
-                        }}
-                      />
-
-                      <PopoverClose>
-                        <Button
-                          variant="outline"
-                          onClick={() => replyToDiscussion(discussion)}
-                        >
-                          Comment
-                          <span className="text-gray-400 ml-2 text-xs">⌘⏎</span>
+                {selectedDiscussionId === discussion.id && (
+                  <div className=" border-t border-gray-200 pt-2">
+                    <Popover
+                      open={activeReplyThreadId === discussion.id}
+                      onOpenChange={(open) => {
+                        open
+                          ? setActiveReplyThreadId(discussion.id)
+                          : setActiveReplyThreadId(null);
+                      }}
+                    >
+                      <PopoverTrigger asChild>
+                        <Button className="mr-2 px-2 h-8" variant="ghost">
+                          <Reply className="mr-2" /> Reply
+                          <span className="text-gray-400 ml-2 text-xs">
+                            (⌘ + ⏎)
+                          </span>
                         </Button>
-                      </PopoverClose>
-                    </PopoverContent>
-                  </Popover>
-                  <Button
-                    variant="outline"
-                    className="select-none h-8 px-2 "
-                    onClick={() => resolveDiscussion(discussion)}
-                  >
-                    <Check className="mr-2" /> Resolve
-                  </Button>
-                </div>
+                      </PopoverTrigger>
+                      <PopoverContent>
+                        <Textarea
+                          className="mb-4"
+                          value={pendingCommentText}
+                          onChange={(event) =>
+                            setPendingCommentText(event.target.value)
+                          }
+                          onKeyDown={(event) => {
+                            if (event.key === "Enter" && event.metaKey) {
+                              replyToDiscussion(discussion);
+                              event.preventDefault();
+                              event.stopPropagation();
+                            }
+                          }}
+                        />
+
+                        <PopoverClose>
+                          <Button
+                            variant="outline"
+                            onClick={() => replyToDiscussion(discussion)}
+                          >
+                            Comment
+                            <span className="text-gray-400 ml-2 text-xs">
+                              (⌘ + ⏎)
+                            </span>
+                          </Button>
+                        </PopoverClose>
+                      </PopoverContent>
+                    </Popover>
+                    <Button
+                      variant="ghost"
+                      className="select-none h-8 px-2 "
+                      onClick={() => resolveDiscussion(discussion)}
+                    >
+                      <Check className="mr-2" /> Resolve
+                      <span className="text-gray-400 ml-2 text-xs">
+                        (⌘ + Y)
+                      </span>
+                    </Button>
+                  </div>
+                )}
               </div>
             );
           })}
