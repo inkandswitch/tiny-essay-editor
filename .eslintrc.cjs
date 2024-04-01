e83e8d2a432f48f16c0f576c1e1207b8297adb3e
Alessandro Warth <alexwarth@gmail.com>
2024-04-01 16:29:04 -0700

    get the linter to shut up about a couple of non-errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,5 +15,14 @@ module.exports = {
       { allowConstantExport: true },
     ],
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ],
+    "no-constant-condition": "off",
   },
 };
