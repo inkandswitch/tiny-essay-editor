39c12940c1cd1498e719b40e2d6c85ab555d74c4
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-04-15 10:29:53 +0200

    monkey patch doc handle to set author and timestamp metadata by default

--- src/DocExplorer/main.tsx
+++ src/DocExplorer/main.tsx
@@ -1,4 +1,5 @@
-import { Repo, AutomergeUrl } from "@automerge/automerge-repo";
+import * as A from "@automerge/automerge/next";
+import { Repo, AutomergeUrl, DocHandle } from "@automerge/automerge-repo";
 import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-network-broadcastchannel";
 import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket";
 
@@ -8,6 +9,7 @@ import { next as Automerge } from "@automerge/automerge";
 import "./index.css";
 import { mount } from "./mount.js";
 import { getAccount } from "./account.js";
+import { timeStamp } from "console";
 
 const repo = new Repo({
   network: [
@@ -15,7 +17,6 @@ const repo = new Repo({
     new BrowserWebSocketClientAdapter("wss://sync.automerge.org"),
   ],
   storage: new IndexedDBStorageAdapter(),
-  changeMetadata: () => ({ author, time: Date.now() }),
 });
 
 let author: AutomergeUrl;
@@ -27,6 +28,37 @@ getAccount(repo).then((account) => {
     author = account.contactHandle.url;
   });
 });
+
+/** monkey patch change and changeAt on doc handle
+ *  always add currently logged in user as author and the current timestamp as metadata to each change
+ *  todo: replace this with a proper meta data api
+ */
+const oldChange = DocHandle.prototype.change;
+DocHandle.prototype.change = function <T>(
+  callback: A.ChangeFn<T>,
+  options: A.ChangeOptions<T> = {}
+) {
+  const optionsWithAttribution: A.ChangeOptions<T> = {
+    time: Date.now(),
+    message: JSON.stringify({ author }),
+    ...options,
+  };
+  oldChange.call(this, callback, optionsWithAttribution);
+};
+
+const oldChangeAt = DocHandle.prototype.changeAt;
+DocHandle.prototype.changeAt = function <T>(
+  heads: A.Heads,
+  callback: A.ChangeFn<T>,
+  options: A.ChangeOptions<T> = {}
+) {
+  const optionsWithAttribution: A.ChangeOptions<T> = {
+    time: Date.now(),
+    message: JSON.stringify({ author }),
+    ...options,
+  };
+  return oldChangeAt.call(this, heads, callback, optionsWithAttribution);
+};
 
 // @ts-expect-error - adding property to window
 window.Automerge = Automerge;
