ea19e76aa85d00ace1f8e8c400ea4556d2fc66bd
Geoffrey Litt <gklitt@gmail.com>
2024-04-30 22:23:20 -0400

    first draft of range slider

--- src/ambsheet/components/ResultHistogram.tsx
+++ src/ambsheet/components/ResultHistogram.tsx
@@ -4,6 +4,10 @@ import { FilterSelection } from './AmbSheet';
 import { Position } from '../datatype';
 import { Histogram } from './Histogram';
 
+import RangeSlider from 'react-range-slider-input';
+import '../range-slider.css';
+import { useEffect, useState } from 'react';
+
 export const ResultHistogram = ({
   selectedCell,
   results,
@@ -24,30 +28,62 @@ export const ResultHistogram = ({
     .map((n) => n.value.rawValue)
     .filter(isNumber);
 
-  return (
-    <Histogram
-      data={numbers}
-      filteredData={filteredNumbers}
-      width={200}
-      height={100}
-      selectValuesBetween={(range) => {
-        if (!range) {
-          setFilterSelectionForCell(selectedCell, null);
-        } else {
-          const numbersToSelect = numbers.filter(
-            (n) => n >= range.min && n <= range.max
-          );
+  const selectValuesBetween = (range) => {
+    if (!range) {
+      setFilterSelectionForCell(selectedCell, null);
+    } else {
+      const numbersToSelect = numbers.filter(
+        (n) => n >= range.min && n <= range.max
+      );
 
-          const indexesToSelect = [];
-          for (let i = 0; i < results.length; i++) {
-            if (numbersToSelect.includes(results[i].value.rawValue as number)) {
-              indexesToSelect.push(i);
-            }
-          }
-
-          setFilterSelectionForCell(selectedCell, indexesToSelect);
+      const indexesToSelect = [];
+      for (let i = 0; i < results.length; i++) {
+        if (numbersToSelect.includes(results[i].value.rawValue as number)) {
+          indexesToSelect.push(i);
         }
-      }}
-    />
+      }
+
+      setFilterSelectionForCell(selectedCell, indexesToSelect);
+    }
+  };
+
+  const numbersMin = Math.min(...numbers);
+  const numbersMax = Math.max(...numbers);
+
+  const [filterBarLimits, setFilterBarLimits] = useState<{
+    min: number;
+    max: number;
+  }>({ min: numbersMin, max: numbersMax });
+
+  useEffect(() => {
+    if (
+      filterBarLimits.min === numbersMin &&
+      filterBarLimits.max === numbersMax
+    ) {
+      selectValuesBetween(null);
+    } else {
+      selectValuesBetween(filterBarLimits);
+    }
+  }, [filterBarLimits]);
+
+  return (
+    <div>
+      <Histogram
+        data={numbers}
+        filteredData={filteredNumbers}
+        width={200}
+        height={100}
+        selectValuesBetween={selectValuesBetween}
+      />
+      <div className="w-[200px] mt-2">
+        <RangeSlider
+          min={numbersMin}
+          max={numbersMax}
+          width={200}
+          value={[filterBarLimits.min, filterBarLimits.max]}
+          onInput={([min, max]) => setFilterBarLimits({ min, max })}
+        />
+      </div>
+    </div>
   );
 };
