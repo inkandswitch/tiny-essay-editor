be6283f7cec13b27a57386518603ac238c8d3969
Peter van Hardenberg <pvh@pvh.ca>
2024-05-15 16:53:38 -0700

    fix datagrid type

--- src/tools/datagrid/index.ts
+++ src/tools/datagrid/index.ts
@@ -2,7 +2,7 @@ import { Tool } from "@/os/tools";
 import { DataGrid } from "./DataGrid";
 
 export default {
-  id: "foo",
+  id: "datagrid",
   name: "Spreadsheet",
   editorComponent: DataGrid,
 } as Tool;
