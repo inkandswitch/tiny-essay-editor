64e82f7b47f3286991c672e49ca6effbc6166c1a
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-06-07 13:09:20 +0200

    replace plugin with simple import map lookup

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,60 @@
 // vite.config.ts
-import { defineConfig } from "vite";
-import path from "path";
 import react from "@vitejs/plugin-react";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 import { globSync } from "glob";
 import { fileURLToPath } from "node:url";
-import externalize from "vite-plugin-externalize-dependencies";
+import path from "path";
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
+
+const IMPORT_MAP = {
+  imports: {
+    "@automerge/automerge":
+      "https://ga.jspm.io/npm:@automerge/automerge@2.2.2/dist/mjs/index.js",
+    "@automerge/automerge-repo":
+      "https://ga.jspm.io/npm:@automerge/automerge-repo@1.1.12/dist/index.js",
+    "@automerge/automerge-repo-network-broadcastchannel":
+      "https://ga.jspm.io/npm:@automerge/automerge-repo-network-broadcastchannel@1.1.12/dist/index.js",
+    "@automerge/automerge-repo-network-messagechannel":
+      "https://ga.jspm.io/npm:@automerge/automerge-repo-network-messagechannel@1.1.12/dist/index.js",
+    "@automerge/automerge-repo-network-websocket":
+      "https://ga.jspm.io/npm:@automerge/automerge-repo-network-websocket@1.1.12/dist/index.js",
+    "@automerge/automerge-repo-react-hooks":
+      "https://ga.jspm.io/npm:@automerge/automerge-repo-react-hooks@1.1.12/dist/index.js",
+    "@automerge/automerge-repo-storage-indexeddb":
+      "https://ga.jspm.io/npm:@automerge/automerge-repo-storage-indexeddb@1.1.12/dist/index.js",
+    "@automerge/automerge-wasm":
+      "https://ga.jspm.io/npm:@automerge/automerge-wasm@0.17.0/bundler/automerge_wasm.js",
+    "@automerge/automerge/next":
+      "https://ga.jspm.io/npm:@automerge/automerge@2.2.2/dist/mjs/next.js",
+    react: "https://ga.jspm.io/npm:react@18.3.1/dev.index.js",
+    "react-dom": "https://ga.jspm.io/npm:react-dom@18.3.1/dev.index.js",
+    "react-dom/client": "https://ga.jspm.io/npm:react-dom@18.3.1/dev.client.js",
+    "react/jsx-dev-runtime":
+      "https://ga.jspm.io/npm:react@18.3.1/dev.jsx-dev-runtime.js",
+    "react/jsx-runtime":
+      "https://ga.jspm.io/npm:react@18.3.1/dev.jsx-runtime.js",
+  },
+};
+
+const EXTERNALS = Object.keys(IMPORT_MAP);
 
 export default defineConfig({
   base: "./",
   plugins: [
     topLevelAwait(),
     react(),
-    externalize({
-      externals: [
-        "react",
-        "@automerge/automerge",
-        "@automerge/automerge-repo",
-        "@automerge/automerge-repo-network-broadcastchannel",
-        "@automerge/automerge-repo-network-messagechannel",
-        "@automerge/automerge-repo-network-websocket",
-        "@automerge/automerge-repo-react-hooks",
-        "@automerge/automerge-repo-storage-indexeddb",
-      ],
-    }),
+    // replace dependencies in import map with url
+    {
+      name: "resolve-external-deps",
+      enforce: "pre",
+      resolveId(source) {
+        if (IMPORT_MAP[source]) {
+          return IMPORT_MAP[source];
+        }
+        return null;
+      },
+    },
   ],
   resolve: {
     alias: {
@@ -38,8 +68,8 @@ export default defineConfig({
     // wrapper has a module level variable to track JS side heap
     // allocations, and initializing this twice causes horrible breakage
     exclude: [
-      "@automerge/automerge-wasm",
-      "@automerge/automerge-wasm/bundler/bindgen_bg.wasm",
+      /* "@automerge/automerge-wasm",
+      "@automerge/automerge-wasm/bundler/bindgen_bg.wasm", */
       "@syntect/wasm",
     ],
   },
@@ -50,11 +80,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: [
-        "@automerge/automerge",
-        "@automerge/automerge-repo-react-hooks",
-        "react",
-      ],
+      external: EXTERNALS,
       input: {
         main: path.resolve(__dirname, "index.html"),
         "service-worker": path.resolve(__dirname, "service-worker.js"),
